1926e3796a9a8323aab0c24b6e0e7fec60d4b646
KIM KYEONG MIN <chocopend@gmail.com>
2019-08-21 15:45:35 +0900

    partials with pug (1)

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 //build
 const app = express();
+app.set("view engine","pug");
 
-//business
-const handleHome = (req,res) =>  res.send("Hello from home");
-const handleProfile = (re,res) => res.send("You are on my profile");
 
 //middleWare
 app.use(cookieParser());
@@ -19,10 +20,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-//routing
-app.get("/",handleHome);
-app.get("/profile",handleProfile);
-
-app.use("/user", userRouter);
+//router
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
