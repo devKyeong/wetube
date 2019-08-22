f4f47aabbb48f71fda2c0067e63f3d3e74d7ac26
KIM KYEONG MIN <chocopend@gmail.com>
2019-08-22 15:31:36 +0900

    Home Controller part One

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 Cloning Youtube with Vanilla and NodeJS
 
 
+## Pages:
 
-제목 : # , =====
-인용 : >
-강조 : * , _
-링크 : [텍스트](주소 "설명 생략가능")
-이미지 : ![텍스트](이미지주소 "설명 생략가능")
-리스트 : 1 , * , - , +
-코드표시 : <code>코드</code> , 한줄 띄우고 스페이스 4칸 , ```코드```
-줄바꿈 : 엔터 2번 , 강제 줄바꿈은 문장끝에 스페이스바 2칸
-가로선 : ----- , ***** , +++++
-
-[출처](https://thisblogbusy.tistory.com/entry/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4Markdown-%EC%9D%B4%EB%9E%80)
+- [ ] Home
+- [X] Join
+- [X] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:342532,
+        title: 'Video awesome',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121234,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },    
+    {
+        id:11111,
+        title: 'Video super',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121234,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },
+    {
+        id:22222,
+        title: 'Video nice',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121234,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },
+    {
+        id:33333,
+        title: 'Video perfect',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121234,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    }
+];
