c937b2d274f0b4f988165909bbfcdebb3466d6ee
KIM KYEONG MIN <chocopend@gmail.com>
2019-08-22 11:06:11 +0900

    Local Variables in Pug

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,15 +11,18 @@ import routes from "./routes";
 
 //build
 const app = express();
-app.set("view engine","pug");
 
 
 //middleWare
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(helmet());//security
+app.set("view engine","pug");//pug view
+app.use(cookieParser());//client->server cookie parsing
+app.use(bodyParser.json());//client->server http post body parsing(json type..)
+app.use(bodyParser.urlencoded({extended:true}));//client->server http post body parsing(form..)
+app.use(morgan("dev"));//logging
+
+app.use(localMiddleware);
+
 
 //router
 app.use(routes.home, globalRouter);
